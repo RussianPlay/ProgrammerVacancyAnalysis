df049646a30de9230148e5c08bc9adc0c5cbb0ae
Elmir <94553249+RussianPlay@users.noreply.github.com>
2026-05-26 22:53:42 +0300

    Исправление ошибок

--- main.py
+++ main.py
@@ -31,7 +31,28 @@
 #
 
 
-from data_collector.VacancyCollector import VacancyCollector
-collector = VacancyCollector(target_vacancies_per_day=5)
-vacancy_ids = collector.collect_vacancies()
-print(len(vacancy_ids))
+# from data_collector.VacancyCollector import VacancyCollector
+# collector = VacancyCollector(target_vacancies_per_day=5)
+# vacancy_ids = collector.collect_vacancies()
+# print(len(vacancy_ids))
+
+
+import requests
+
+client_id = "T0UT7PLJVSL453O29JRPLLHL8N1H8233BCB7OI9HJO3CIKITRFV0UO06R0GDCFNQ"
+client_secret = "RUJPB2LCE4HDN2EF86R6KKH3M86FV5RSLQFRCGAEIHEHOLRUT6Q20Q4FQ8FAOOGV"
+auth_code = "PHE8488PBQIJ4KMT4V56858EID4H7VN2BG99JRH2O1AQJ5SPUTNRT662IHEDSCUH"
+redirect_uri = "http://localhost:8080"
+
+url = "https://hh.ru/oauth/token"
+body = {
+        'grant_type': 'authorization_code',
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': auth_code,
+        'redirect_uri': redirect_uri }
+
+response = requests.post(url, data=body)
+# {'access_token': 'USERI536KBQ5IL4LDQFLOV42CVF0UDQD8IVPC78UCTA46LEJ8DC113249OQ8UR63', 'token_type': 'bearer', 'refresh_token': 'USERP9QNS3EI7KGE5GTRJDGULTP2BM4KEQA7856A42RCOG9EC2G74EO7SBJMU50D', 'expires_in': 1209599}
+tokens = response.json()
+print(tokens)
